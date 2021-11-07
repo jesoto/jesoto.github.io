6764801478933cc40fa7d084717e99bc4e938eb0
Jesús Soto Enriquez <jesus.soto@pucp.edu.pe>
2021-11-07 02:44:50 -0500

    Add files via upload

---
+++ portafolio/src/app.js
@@ -0,0 +1,17 @@
+const hamburgerButton = document.getElementById('hamburger')
+const navList = document.getElementById('nav-list')
+
+function toogleButton() {
+    navList.classList.toggle('show')
+}
+
+hamburgerButton.addEventListener('click',toogleButton)
+
+
+
+
+
+
+
+
+
